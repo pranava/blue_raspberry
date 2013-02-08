5917870e94a44170f8487d1dca84f7a79e090dcf
Pranava Adduri <pranava@Pranavas-MacBook-Air.local>
2013-02-07 16:27:20 -0800

    Initial commit of project blue_raspberry

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlueRaspberry::Application.config.secret_token = '0d0a753d07df5c9ab5ddfecf6b2c0ee3ff4fa2917757a290f62a0bb0a67a21f88df9d829a19666969a5f7ccb02c6145742db14647c151ad88361a6deda8a4512'
